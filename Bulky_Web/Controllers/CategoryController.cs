7fc8efa7d19f5813ab5269c3e82b2b8642dfb975
Ashkan <152452003+AshkanSo@users.noreply.github.com>
2024-05-12 23:54:14 +0330

    Add project files.

---
+++ Bulky_Web/Controllers/CategoryController.cs
@@ -0,0 +1,73 @@
+﻿using Bulky_Web.Data;
+using Bulky_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Bulky_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Connot Add - Name and Display Order is Same!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+           else
+            {
+                return View();
+            }
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Connot Add - Name and Display Order is Same!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Bulky_Web/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky_Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Category")]
+        [Range(1,100, ErrorMessage ="لفطا بین عدد 1 تا 100 انتخاب کنید")]
+        public int DisplayOrder { get; set; }
+    }
+}
